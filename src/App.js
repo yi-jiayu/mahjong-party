df3cd8b425b8f40a851bdda8c92ce73d20ab32bd
Jiayu Yi <yijiayu@gmail.com>
2020-08-08 15:46:59 +0800

    Extract some components into own files

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import {
   useHistory
 } from "react-router-dom";
 
-import Board from './Board';
+import Room from './Room';
+import NotFound from "./NotFound";
 
 class App extends React.Component {
   constructor(props) {
@@ -27,11 +28,11 @@ class App extends React.Component {
         <Router>
           <div>
             <Switch>
-              <Route path="/rooms">
-                <Rooms/>
-              </Route>
               <Route exact path="/">
                 <Home self={this.state.self}/>
+              </Route>
+              <Route path="/rooms">
+                <Rooms/>
               </Route>
               <Route path="*">
                 <NotFound/>
@@ -41,10 +42,6 @@ class App extends React.Component {
         </Router>
     );
   }
-}
-
-function NotFound() {
-  return <h1>Not Found</h1>;
 }
 
 function JoinRoom() {
@@ -86,59 +83,6 @@ function Home({self}) {
     </div>
     <JoinRoom/>
   </>;
-}
-
-function Lobby({roomId, players}) {
-  const startGame = async () => {
-    await fetch(`http://localhost:8080/rooms/${roomId}/start`, {method: 'post', credentials: "include"});
-  }
-
-  return <>
-    <h1>{roomId}</h1>
-    <p>Current players:</p>
-    <ul>
-      {players.map(p => <li key={p}>{p}</li>)}
-    </ul>
-    {players.length === 4 && <button onClick={startGame}>Start game</button>}
-  </>;
-}
-
-function Room() {
-  const {roomId} = useParams();
-  const [players, setPlayers] = useState([]);
-  const [phase, setPhase] = useState(0);
-  const [round, setRound] = useState(null);
-  const [self, setSelf] = useState({});
-
-  useEffect(() => {
-    const eventSource = new EventSource(`http://localhost:8080/rooms/${roomId}/live`);
-    eventSource.onerror = () => {
-      if (eventSource.readyState === EventSource.CLOSED) {
-        setPhase(-1);
-      }
-    }
-    eventSource.onmessage = async e => {
-      const {phase, players, round} = JSON.parse(e.data);
-      setPlayers(players);
-      setRound(round);
-      setPhase(phase);
-      if (phase === 1) {
-        const resp = await fetch(`http://localhost:8080/rooms/${roomId}/self`, {credentials: "include"})
-        const self = await resp.json();
-        setSelf(self);
-      }
-    };
-    return () => eventSource.close();
-  }, [roomId])
-
-  switch (phase) {
-    case 0:
-      return <Lobby roomId={roomId} players={players}/>;
-    case 1:
-      return <Board players={players} round={round} self={self}/>
-    default:
-      return <NotFound/>
-  }
 }
 
 function Rooms() {
