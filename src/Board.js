fe45789355d6c65edca7a75121b53df1b486df39
Jiayu Yi <yijiayu@gmail.com>
2020-08-08 23:23:46 +0800

    Implement chowing

--- src/Board.js
+++ src/Board.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import './board.css';
 import './tiles.css';
 import * as mahjong from './mahjong';
 
 const DIRECTIONS = ['East', 'South', 'West', 'North']
 
-function Rack({tiles, onClick}) {
-  return <div className="rack">
-    {tiles.map((tile, index) => <span className="tile"
+function InteractiveRack({tiles, onClick, selecting, selected}) {
+  return <div className={selecting ? "rack selecting" : "rack"}>
+    {tiles.map((tile, index) => <span className={selected === index ? "tile selected" : "tile"}
                                       data-tile={tile}
                                       key={tile + index}
-                                      onClick={() => onClick(tile)}/>)}
+                                      onClick={() => onClick(tile, index)}/>)}
+  </div>;
+}
+
+function Rack({tiles}) {
+  return <div className="rack">
+    {tiles.map((tile, index) => <span className="tile" data-tile={tile} key={tile + index}/>)}
   </div>;
 }
 
@@ -31,17 +37,46 @@ function Board({self, players, round, doAction}) {
   const order = [seat, (seat + 1) % 4, (seat + 2) % 4, (seat + 3) % 4];
   const [bottom, right, top, left] = order.map(x => ({direction: DIRECTIONS[x], name: players[x], ...hands[x]}));
 
+  const [selecting, setSelecting] = useState(false);
+  const [selected, setSelected] = useState(-1);
+  const [pendingAction, setPendingAction] = useState('');
+
+  const selectTilesForChow = () => {
+    setSelecting(true);
+    setPendingAction('chow');
+  };
+
   const canDiscard = current_turn === seat && current_action === mahjong.ACTION_DISCARD
   const canDraw = current_turn === seat && current_action === mahjong.ACTION_DRAW;
 
   let message = '';
   if (canDiscard) {
     message = 'Select a tile to discard';
+  } else if (selecting && pendingAction === 'chow') {
+    message = 'Select two tiles to chow with'
   }
 
-  const tileClick = tile => {
+  const tileClick = (tile, index) => {
     if (canDiscard) {
       doAction('discard', [tile]);
+      return;
+    }
+    if (!selecting) {
+      return;
+    }
+    if (selected === index) {
+      setSelected(-1);
+      return;
+    }
+    if (selected === -1) {
+      setSelected(index);
+      return
+    }
+    if (pendingAction === 'chow') {
+      doAction('chow', [self.concealed[selected], self.concealed[index]]);
+      setSelecting(false);
+      setSelected(-1);
+      setPendingAction('');
     }
   };
   const drawTile = () => doAction('draw', []);
@@ -59,13 +94,13 @@ function Board({self, players, round, doAction}) {
           <div className="bottom">
             <Rack tiles={bottom.flowers}/>
             <Rack tiles={bottom.revealed}/>
-            <Rack tiles={bottom.concealed} onClick={tileClick}/>
+            <InteractiveRack tiles={bottom.concealed} onClick={tileClick} selecting={selecting} selected={selected}/>
           </div>
           <div className="message">{message}</div>
           <div className="actions">
             <div>
               <button onClick={drawTile} disabled={!canDraw}>Draw tile</button>
-              <button disabled>Chow</button>
+              <button disabled={!canDraw} onClick={selectTilesForChow}>Chow</button>
               <button disabled>Peng</button>
               <button disabled>Kong</button>
               <button disabled>Declare win</button>
