6b1322ecad015df8a7c174d831bfe096edd999d7
Jiayu Yi <yijiayu@gmail.com>
2020-08-15 23:05:38 +0800

    Fix missing import

--- src/Room.js
+++ src/Room.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import NotFound from "./NotFound";
-import Board from "./Board";
+import Board, { Rack } from "./Board";
 import ReadOnlyBoard from "./ReadOnlyBoard";
 
 function Lobby({roomId, players, doAction}) {
