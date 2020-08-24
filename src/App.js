aeb37c3950e56b6ea6a6af24c7b49f777f04288c
Jiayu Yi <yijiayu@gmail.com>
2020-08-24 16:55:38 +0800

    Support per-room names
    - Added TypeScript
    - Restyled home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import {
   BrowserRouter as Router,
-  Switch,
   Route,
+  Switch,
   useRouteMatch,
 } from "react-router-dom";
 
@@ -12,49 +12,36 @@ import Room from "./Room";
 import NotFound from "./NotFound";
 import Tutorial from "./Tutorial";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { self: {} };
-  }
-
-  async componentDidMount() {
-    const resp = await fetch("/api/self", { credentials: "include" });
-    const self = await resp.json();
-    this.setState({ self });
-  }
-
-  render() {
-    return (
-      <Router>
-        <Route
-          path="/"
-          render={({ location }) => {
-            if (typeof window.ga === "function") {
-              window.ga("set", "page", location.pathname + location.search);
-              window.ga("send", "pageview");
-            }
-          }}
-        />
-        <div>
-          <Switch>
-            <Route exact path="/">
-              <Home self={this.state.self} />
-            </Route>
-            <Route path="/rooms">
-              <Rooms />
-            </Route>
-            <Route path="/tutorial">
-              <Tutorial />
-            </Route>
-            <Route path="*">
-              <NotFound />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <Router>
+      <Route
+        path="/"
+        render={({ location }) => {
+          if (typeof window.ga === "function") {
+            window.ga("set", "page", location.pathname + location.search);
+            window.ga("send", "pageview");
+          }
+        }}
+      />
+      <div>
+        <Switch>
+          <Route exact path={["/", "/host", "/join", "/about"]}>
+            <Home />
+          </Route>
+          <Route path="/rooms">
+            <Rooms />
+          </Route>
+          <Route path="/tutorial">
+            <Tutorial />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 function Rooms() {
---
+++ src/Home.tsx
@@ -0,0 +1,148 @@
+import React, { SyntheticEvent, useState } from "react";
+import { Link, Route, Switch, useHistory } from "react-router-dom";
+
+import styles from "./Home.module.css";
+
+function HostGame() {
+  const history = useHistory();
+  const [name, setName] = useState("");
+
+  const hostGame = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    const resp = await fetch("/api/rooms", {
+      method: "post",
+      credentials: "include",
+      body: `name=${name}`,
+      headers: { "content-type": "application/x-www-form-urlencoded" },
+    });
+    const { room_id: roomId } = await resp.json();
+    history.replace(`/rooms/${roomId}`);
+  };
+
+  return (
+    <>
+      <h2>Host a new game</h2>
+      <form onSubmit={hostGame}>
+        <label htmlFor="input_HostGame_name">Your name</label>
+        <input
+          type="text"
+          name="name"
+          id="input_HostGame_name"
+          onChange={(e) => setName(e.target.value)}
+          pattern="[0-9A-Za-z]+"
+          required
+        />
+        <div>
+          <input type="submit" value="Host" />
+        </div>
+      </form>
+      <Link to="/">Back</Link>
+    </>
+  );
+}
+
+function JoinGame() {
+  const history = useHistory();
+
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  const joinGame = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    const resp = await fetch(`/api/rooms/${room}`, {
+      method: "post",
+      credentials: "include",
+      body: `name=${encodeURIComponent(name)}`,
+      headers: { "content-type": "application/x-www-form-urlencoded" },
+    });
+    if (resp.status === 200) {
+      history.replace(`/rooms/${room}`);
+    }
+  };
+
+  return (
+    <>
+      <h2>Join an existing game</h2>
+      <form onSubmit={joinGame}>
+        <label htmlFor="input_JoinGame_name">Your name</label>
+        <input
+          type="text"
+          name="name"
+          id="input_HostGame_name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label htmlFor="input_JoinGame_roomId">Room ID</label>
+        <input
+          type="text"
+          name="room"
+          id="input_HostGame_room"
+          onChange={(e) => setRoom(e.target.value)}
+        />
+        <div>
+          <input type="submit" value="Join" />
+        </div>
+      </form>
+      <Link to="/">Back</Link>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <h2>About Mahjong Party</h2>
+      <p>Mahjong Party is a work in progress.</p>
+      <p>
+        Find Mahjong Party on GitHub:{" "}
+        <a href="https://github.com/yi-jiayu/mahjong-party">
+          https://github.com/yi-jiayu/mahjong-party
+        </a>
+        .
+      </p>
+      <p>
+        All mahjong tile images are sourced from{" "}
+        <a
+          href="https://commons.wikimedia.org/wiki/Category:SVG_Oblique_illustrations_of_Mahjong_tiles"
+          title="via Wikimedia Commons">
+          Cangjie6
+        </a>{" "}
+        / <a href="https://creativecommons.org/licenses/by-sa/4.0">CC BY-SA</a>.
+      </p>
+      <Link to="/">Back</Link>
+    </>
+  );
+}
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <Switch>
+        <Route exact path="/">
+          <div className={styles.party}>
+            <span role="img" aria-label="Mahjong Party">
+              🀄🎉
+            </span>
+            <span role="img" aria-label="Mahjong Party">
+              🎈🎊
+            </span>
+          </div>
+          <h1>Mahjong Party</h1>
+          <p>Play Singaporean mahjong online with friends!</p>
+          <Link to="/tutorial">Tutorial</Link>
+          <Link to="/host">Host game</Link>
+          <Link to="/join">Join game</Link>
+          <Link to="/about">About</Link>
+        </Route>
+        <Route path="/host">
+          <HostGame />
+        </Route>
+        <Route path="/join">
+          <JoinGame />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+      </Switch>
+    </main>
+  );
+}
